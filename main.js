ce877cc49f8b1dd93a56d78a34d079d7c7b5668c
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-06-13 09:37:52 +0500

    complete work

---
+++ main.js
@@ -0,0 +1,32 @@
+let input = document.querySelector('#search')
+let btn = document.querySelector('#btn')
+let copyBtn = document.querySelector('#copy')
+let content = document.querySelector('.content')
+
+btn.addEventListener('click', loremGen)
+
+copyBtn.addEventListener('click',  ()=> {
+    navigator.clipboard.writeText(content.textContent).then(()=> {
+        alert('Copy text succesfully')
+    })
+})
+
+async function loremGen() {
+    try {
+        if (input.value == '') {
+            alert('Requird')
+        } else {
+            input.value = ''
+            let responce = await fetch("http://hipsum.co/api/?type=hipster-centric&sentences=3")
+            content.style.display = 'block'
+            let data = await responce.json()
+            console.log(data);
+            content.textContent = data
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+
